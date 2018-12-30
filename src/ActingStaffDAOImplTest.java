1e188a32a8c4ca9cc83075a163dc6da0681d30ac
biucell <799162425@qq.com>
2018-12-30 19:41:56 +0800

    ActingStaff-Junit

---
+++ src/ActingStaffDAOImplTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ActingStaffDAOImplTest {
+	private static ActingStaffDAOImpl actingStaffImpl=new ActingStaffDAOImpl();
+		
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testSearchActingStaff() {
+		//fail("Not yet implemented");
+		actingStaffImpl.searchActingStaff(15);
+	}
+
+}
