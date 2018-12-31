ae21fa28ef6acb961d12edc9054b984cc52603ec
biucell <799162425@qq.com>
2018-12-31 10:49:28 +0800

    add Movie.java

---
+++ src/Movie.java
@@ -0,0 +1,166 @@
+
+public class Movie {
+	int movieId;
+	String chineseName;
+	String foreignName;
+	String movieType;
+	String countryregion;
+	String language;
+	String releaseDate;
+	int movieLength;
+	String alternateName;
+	String imdbLink;
+	float score;
+	String plotSummary;
+	int releaseYear;
+	int oneStar;
+	int twoStar;
+	int threeStar;
+	int fourStar;
+	int fiveStar;
+	
+	public int getMovieId(){
+		return this.movieId;
+	}
+	
+	public void setMovieId(int id){
+		this.movieId=id;
+	}
+	
+	public String getChineseName(){
+		return this.chineseName;
+	}
+	
+	public void setChineseName(String name){
+		this.chineseName=name;
+	}
+	
+	public String getForeignName(){
+		return this.foreignName;
+	}
+	
+	public void setForeignName(String name){
+		this.foreignName=name;
+	}
+	
+	public String getMovieType(){
+		return this.movieType;
+	}
+	
+	public void setMovieType(String type){
+		this.movieType=type;
+	}
+	
+	public String getCountryregion(){
+		return this.countryregion;
+	}
+	
+	public void setCountryregion(String countryregion){
+		this.countryregion=countryregion;
+	}
+	
+	public String getLanguage(){
+		return this.language;
+	}
+	
+	public void setLanguage(String language){
+		this.language=language;
+	}
+	
+	public String getReleaseDate(){
+		return this.releaseDate;
+	}
+	
+	public void setReleaseDate(String date){
+		this.releaseDate=date;
+	}
+	
+	public int getMovieLength(){
+		return this.movieLength;
+	}
+	
+	public void setMovieLength(int length){
+		this.movieLength=length;
+	}
+	
+	public String getAlternateName(){
+		return this.alternateName;
+	}
+	
+	public void setAlternateName(String name){
+		this.alternateName=name;
+	}
+	
+
+	public String getImdbLink(){
+		return this.imdbLink;
+	}
+	
+	public void setImdbLink(String link){
+		this.imdbLink=link;
+	}
+	
+	public float getScore(){
+		return this.score;
+	}
+	
+	public void setScore(float score){
+		this.score=score;
+	}
+	
+	public String getPlotSummary(){
+		return this.plotSummary;
+	}
+	
+	public void setPlotSummary(String plot){
+		this.plotSummary=plot;
+	}
+	
+	public int getReleaseYear(){
+		return this.releaseYear;
+	}
+	
+	public void setReleaseYear(int year){
+		this.releaseYear=year;
+	}
+	
+	public int getOneStar(){
+		return this.oneStar;
+	}
+	
+	public void setOneStar(int star){
+		this.oneStar=star;
+	}
+	
+	public int getTwoStar(){
+		return this.twoStar;
+	}
+	
+	public void setTwoStar(int star){
+		this.twoStar=star;
+	}
+	
+	public int getThreeStar(){
+		return this.threeStar;
+	}
+	
+	public void setThreeStar(int star){
+		this.threeStar=star;
+	}
+	
+	public int getFourStar(){
+		return this.fourStar;
+	}
+	
+	public void setFourStar(int star){
+		this.fourStar=star;
+	}
+	
+	public int getFiveStar(){
+		return this.fiveStar;
+	}
+	
+	public void setFiveStar(int star){
+		this.fiveStar=star;
+	}
+}
