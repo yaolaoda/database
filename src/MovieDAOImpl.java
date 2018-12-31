bb1cb91509a4191d05118f8d8a8c8f6c3494e461
biucell <799162425@qq.com>
2018-12-31 11:01:33 +0800

    update MovieDAOImpl.java

--- src/MovieDAOImpl.java
+++ src/MovieDAOImpl.java
@@ -18,24 +18,12 @@ public class MovieDAOImpl extends DAOBase implements MovieDAO {
 			ps.setString(1, "%"+keyword+"%");
 			rs=ps.executeQuery();
 			while(rs.next()){
-				System.out.println("movieId:"+rs.getInt("movieId"));
 				System.out.println("chineseName:"+rs.getString("chineseName"));
 				System.out.println("foreignName:"+rs.getString("foreignName"));
 				System.out.println("movieType:"+rs.getString("movieType"));
 				System.out.println("country/region:"+rs.getString("country/region"));
-				System.out.println("language:"+rs.getString("language"));
 				System.out.println("releaseDate:"+rs.getString("releaseDate"));
-				System.out.println("movieLength:"+rs.getInt("movieLength"));
-				System.out.println("alternateName:"+rs.getString("alternateName"));
-				System.out.println("imdbLink:"+rs.getString("imdbLink"));
-				System.out.println("score:"+rs.getFloat("score"));
-				System.out.println("plotSummary:"+rs.getString("plotSummary"));
 				System.out.println("releaseYear:"+rs.getInt("releaseYear"));
-				System.out.println("oneStar:"+rs.getInt("oneStar"));
-				System.out.println("twoStar:"+rs.getInt("twoStar"));
-				System.out.println("threeStar:"+rs.getInt("threeStar"));
-				System.out.println("fourstar:"+rs.getInt("fourStar"));
-				System.out.println("fiveStar:"+rs.getInt("fiveStar"));
 				System.out.println();
 			}
 		}catch(Exception e){
